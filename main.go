be51a8e71d616bc628a0fc537d5b29aa2fab41d3
rigensen <mr.right.0005@gmail.com>
2023-06-23 14:51:37 +0800

    update

--- main.go
+++ main.go
@@ -7,12 +7,12 @@ import (
 
 func main() {
 	// 输入贷款本金、年利率、还款期限
-	principal := 500000.0       // 贷款本金
-	interestRate := 0.05 / 12   // 年利率，月利率 = 年利率 / 12
-	paymentPeriod := 30 * 12    // 还款期限，以月为单位
-	paymentPerMonth := 0.0     // 每月还款额
-	totalInterest := 0.0       // 总利息
-	totalPayment := 0.0        // 还款总额
+	principal := 500000.0     // 贷款本金
+	interestRate := 0.05 / 12 // 年利率，月利率 = 年利率 / 12
+	paymentPeriod := 30 * 12  // 还款期限，以月为单位
+	paymentPerMonth := 0.0    // 每月还款额
+	totalInterest := 0.0      // 总利息
+	totalPayment := 0.0       // 还款总额
 
 	// 计算每月还款额
 	paymentPerMonth = (principal * interestRate * math.Pow(1+interestRate, float64(paymentPeriod))) / (math.Pow(1+interestRate, float64(paymentPeriod)) - 1)
@@ -22,10 +22,13 @@ func main() {
 	totalPayment = paymentPerMonth * float64(paymentPeriod)
 
 	// 输出计算结果
-	fmt.Printf("每月还款额为：%.2f 元
-", paymentPerMonth)
-	fmt.Printf("总利息为：%.2f 元
-", totalInterest)
-	fmt.Printf("还款总额为：%.2f 元
-", totalPayment)
+	fmt.Printf("每月还款额为：%.2f 元\n", paymentPerMonth)
+	fmt.Printf("总利息为：%.2f 元\n", totalInterest)
+	fmt.Printf("还款总额为：%.2f 元\n", totalPayment)
+
+	// 打印每个月的月供
+	for i := 1; i <= paymentPeriod; i++ {
+		monthPayment := (principal-interestRate*float64(i-1))*paymentPerMonth - (principal * interestRate)
+		fmt.Printf("第 %d 个月月供为：%.2f 元\n", i, monthPayment)
+	}
 }
