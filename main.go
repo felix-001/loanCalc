7cf8a308b05de84b3be6d5ceede4dd0f73614dba
rigensen <mr.right.0005@gmail.com>
2023-06-24 18:03:06 +0800

    第一版计算提前还款是ok的

--- main.go
+++ main.go
@@ -12,14 +12,16 @@ func main() {
 	var months int
 	var rate float64
 	var start int
-	var extraPayment float64
+	var prepayment float64
+	var duration int
 
 	flag.Float64Var(&principal, "p", 1140000.0, "贷款本金")
 	flag.IntVar(&years, "y", 20, "贷款年限")
 	flag.Float64Var(&rate, "r", 0.0475, "贷款年利率")
 	flag.IntVar(&months, "m", 0, "贷款月数")
 	flag.IntVar(&start, "s", 1, "已还月数")
-	flag.Float64Var(&extraPayment, "e", 0, "提前还款金额")
+	flag.Float64Var(&prepayment, "e", 300000.0, "提前还款金额")
+	flag.IntVar(&duration, "d", 12, "每隔多少个月提前还款")
 	flag.Parse()
 
 	interestRate := rate / 12   // 年利率，月利率 = 年利率 / 12
@@ -41,16 +43,19 @@ func main() {
 	totalPayment = paymentPerMonth * float64(paymentPeriod)
 
 	// 输出计算结果
-	fmt.Println("每月还款额,总利息,还款总额")
+	fmt.Println("每月还款额(元), 总利息(元), 还款总额(元)")
 	fmt.Printf("%.2f, %.2f, %.2f\n", paymentPerMonth, totalInterest, totalPayment)
 
-	principalPerMon := (principal * interestRate * math.Pow(1+interestRate, float64(11-1))) /
-		(math.Pow(1+interestRate, float64(paymentPeriod)) - 1)
-	fmt.Printf("第11个月的本金: %.2f\n", principalPerMon)
+	//principalPerMon := (principal * interestRate * math.Pow(1+interestRate, float64(11-1))) /
+	//	(math.Pow(1+interestRate, float64(paymentPeriod)) - 1)
+	//fmt.Printf("第11个月的本金: %.2f\n", principalPerMon)
 
-	fmt.Println("期,月供,本金,利息")
+	cnt := 1
+	left := principal
+	fmt.Println("期, 月供(元), 本金(元), 利息(元), 剩余贷款(元)")
 	// 打印每个月的月供和利息
 	for i := start; i <= paymentPeriod; i++ {
+		extra := 0
 		// 每月应还本金=贷款本金×月利率×(1+月利率)^(还款月序号-1)÷〔(1+月利率)^还款月数-1〕
 		principalPerMon := (principal * interestRate * math.Pow(1+interestRate, float64(i-1))) /
 			(math.Pow(1+interestRate, float64(paymentPeriod)) - 1)
@@ -59,6 +64,19 @@ func main() {
 		monthInterest := principal * interestRate * (math.Pow(1+interestRate,
 			float64(paymentPeriod)) - math.Pow(1+interestRate, float64(i-1))) /
 			(math.Pow(1+interestRate, float64(paymentPeriod)) - 1)
-		fmt.Printf("%03d,%.2f元,%.2f元,%.2f元\n", i, paymentPerMonth, principalPerMon, monthInterest)
+		// 剩余贷款
+		left -= principalPerMon
+		if i == duration {
+			extra = int(prepayment)
+			paymentPeriod -= i
+			left -= prepayment
+			principal = left
+			paymentPerMonth = (principal * interestRate * math.Pow(1+interestRate,
+				float64(paymentPeriod))) / (math.Pow(1+interestRate, float64(paymentPeriod)) - 1)
+			i = 0
+		}
+
+		fmt.Printf("%03d, %.2f, %.2f, %.2f, %.2f\n", cnt, paymentPerMonth+float64(extra), principalPerMon, monthInterest, left)
+		cnt++
 	}
 }
